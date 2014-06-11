741fce9fee4d494d4aebd7e6bf9f076f7de98ede
Tim Hamer <tim@arcaexterna.com>
2014-06-11 15:54:51 -0500

    log in to cellarhq

--- src/com/arcaexterna/Cellarhq/LoginActivity.java
+++ src/com/arcaexterna/Cellarhq/LoginActivity.java
@@ -1,9 +1,25 @@
 package com.arcaexterna.Cellarhq;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoginActivity extends Activity {
     /**
@@ -16,6 +32,64 @@ public class LoginActivity extends Activity {
     }
 
     public void loginButtonClicked(View view) {
+
+        AsyncTask<String, Void, Void> loginTask = new AsyncTask<String, Void, Void>() {
+            @Override
+            protected Void doInBackground(String... strings) {
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpPost httppost = new HttpPost("http://www.cellarhq.com/signin/processEmail");
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+                nameValuePairs.add(new BasicNameValuePair("email", strings[0]));
+                nameValuePairs.add(new BasicNameValuePair("password", strings[1]));
+                try {
+                    httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    HttpResponse response = httpclient.execute(httppost);
+                    if (response.getStatusLine().getStatusCode() == 200) {
+                        loginSuccess();
+                    } else {
+                        loginFailure();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+
+            }
+        };
+        String user = ((EditText)findViewById(R.id.usernameField)).getText().toString();
+        String password = ((EditText)findViewById(R.id.passwordField)).getText().toString();
+        if (user.isEmpty() || password.isEmpty()) {
+            loginFailure();
+        } else {
+            loginTask.execute(user, password);
+        }
+
+    }
+
+    private void loginFailure() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, AlertDialog.THEME_DEVICE_DEFAULT_LIGHT);
+        builder.setTitle("Login Failed");
+        builder.setMessage("Failed to log in. Please try again");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void loginSuccess() {
         Intent cellarIntent = new Intent(LoginActivity.this, CellarActivity.class);
         startActivity(cellarIntent);
     }
